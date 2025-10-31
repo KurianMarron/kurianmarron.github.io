453a0bb8308626a0d8ac56625263fd8418b819cc
KurianMarron <kurianmarron@gmail.com>
2025-10-31 23:57:47 +0900

    ヘッダーとフッターにSNSへのリンク追加

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,10 +1,22 @@
-// フッター
+import { FaXTwitter, FaBluesky, FaGithub } from 'react-icons/fa6';
 
+// フッター
 function Footer() {
   return (
     <footer className="mt-auto w-full border-t border-gray-300 **:text-gray-500">
-      <div className="flex justify-center">
-        <div className="flex flex-col gap-4 items-center container p-8">
+      <div className="flex justify-center p-8">
+        <div className="flex flex-col gap-8 items-center container">
+          <div className="flex gap-8">
+            <a href="https://x.com/KurianMarron" target="_blank">
+              <FaXTwitter size={24}/>
+            </a>
+            <a href="https://bsky.app/profile/kurianmarron.bsky.social" target="_blank">
+              <FaBluesky size={24}/>
+            </a>
+            <a href="https://github.com/KurianMarron" target="_blank">
+              <FaGithub size={24}/>
+            </a>
+          </div>
           <div>© 2025 KurianMarron</div>
         </div>
       </div>
--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,11 +1,23 @@
-// ヘッダー
+import { FaXTwitter, FaBluesky, FaGithub } from 'react-icons/fa6';
 
+// ヘッダー
 function Header() {
   return (
-    <header className="w-full border-b border-gray-300">
-      <div className="flex justify-center">
-        <div className="flex container">
-          <div className="text-2xl p-4">栗庵／Kurian</div>
+    <header className="w-full border-b border-gray-300 **:text-gray-800">
+      <div className="flex justify-center p-4">
+        <div className="flex justify-between items-center container">
+          <div className="text-2xl">Kurian's Portfolio</div>
+          <div className="hidden md:flex gap-8">
+            <a href="https://x.com/KurianMarron" target="_blank">
+              <FaXTwitter size={24}/>
+            </a>
+            <a href="https://bsky.app/profile/kurianmarron.bsky.social" target="_blank">
+              <FaBluesky size={24}/>
+            </a>
+            <a href="https://github.com/KurianMarron" target="_blank">
+              <FaGithub size={24}/>
+            </a>
+          </div>
         </div>
       </div>
     </header>
